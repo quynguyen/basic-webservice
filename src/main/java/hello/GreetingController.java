c5a5a4e5c50af2d53d28103ec15888a18db8e20a
Quy <quy.nguyen@gmail.com>
2014-02-11 02:12:59 -0800

    First commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class GreetingController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public
+    @ResponseBody
+    Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return new Greeting(counter.getAndIncrement(), String.format(template, name));
+    }
+}
